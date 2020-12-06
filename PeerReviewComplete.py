fc967eba6d17b510b6c932a37dce9befd7cc9f24
chmn-meow <73254717+chmn-meow@users.noreply.github.com>
2020-12-06 17:26:19 -0600

    Completed assignement

--- PeerReviewComplete.py
+++ PeerReviewComplete.py
@@ -1,54 +1,48 @@
-# This is a header for the application
-# You should read this header and insert your name and your date below as part of the peer review
-# This is a typical part of any program
-# Author: <author>
-# Creation Date: <date>
-# Below is a simple program with 10 issues (some are syntax errors and some are logic errors.  You need to identify the issues and correct them.
+# Peer Review Author: Jayson Davidson
+# Peer Review Date: 06-Dec-20
 
 import random
 import time
 
-def displayIntro():
-	print('''You are in a land full of dragons. In front of you,
-	you see two caves. In one cave, the dragon is friendly
-	and will share his treasure with you. The other dragon
-	is greedy and hungry, and will eat you on sight.''')
-	print()
 
-def chooseCave():
-    cave = ''
-	while cave != '1' and cave != '2':
-		print('Which cave will you go into? (1 or 2)')
-		cave = input()
+def display_intro():
+    intro = "You are in a land full of dragons. In front of you,\nyou see two caves. In one cave, the dragon is friendly\nand will share his treasure with you. The other dragon\nis greedy and hungry, and will eat you on sight."
+    print(f"{intro}\n")
 
-	return caves
 
-def checkCave(chosenCave):
-	print('You approach the cave...')
-	#sleep for 2 seconds
-	time.sleep(2)
-	print('It is dark and spooky...')
-	#sleep for 2 seconds
-	time.sleep(3)
-	print('A large dragon jumps out in front of you! He opens his jaws and...')
-	print()
-	#sleep for 2 seconds
-	time.sleep(2)
-	friendlyCave = random.randint(1, 2)
+def choose_cave():
+    cave = None
+    while cave != 1 and cave != 2:
+        print("Which cave will you go into? (1 or 2)")
+        cave = int(input())
+    return cave
 
-	if chosenCave == str(friendlyCave):
-		print('Gives you his treasure!')
-	else:
-		print 'Gobbles you down in one bite!'
 
-playAgain = 'yes'
-while playAgain = 'yes' or playAgain = 'y':
-	displayIntro()
-	caveNumber = choosecave()
-	checkCave(caveNumber)
-    
-	print('Do you want to play again? (yes or no)')
-	playAgain = input()
-	if playAgain == "no":
-		print("Thanks for planing")
+def check_cave(chosen_cave):
+    print("You approach the cave...")
+    # sleep for 2 seconds
+    time.sleep(2)
+    print("It is dark and spooky...")
+    # sleep for 2 seconds
+    time.sleep(2)
+    print("A large dragon jumps out in front of you! He opens his jaws and...\n")
+    # sleep for 2 seconds
+    time.sleep(2)
+    frinedly_cave = random.randint(1, 2)
 
+    if chosen_cave == frinedly_cave:
+        print("Gives you his treasure!")
+    else:
+        print("Gobbles you down in one bite!")
+
+
+play_again = "yes"
+while play_again == "yes" or play_again == "y":
+    display_intro()
+    cave_number = choose_cave()
+    check_cave(cave_number)
+
+    print("Do you want to play again? (yes or no)")
+    play_again = input()
+    if play_again == "no" or play_again == "n":
+        print("Thanks for playing!")
